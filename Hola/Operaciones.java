65c333c48abb10024c075742fd82333a0bdf9f08
Yerik <Xan-Tel@hotmail.com>
2020-09-08 18:56:14 -0500

    Primer Commit

---
+++ Hola/Operaciones.java
@@ -0,0 +1,15 @@
+package operaciones;
+public class Operaciones {
+    public static void main(String[] args) {
+        
+        int X = 5;
+        int Y = 2;
+        
+        System.out.println(X + "\n" + Y);
+        System.out.println("Suma: " + (X+Y) );
+        System.out.println("Resta: " + (X-Y));
+        System.out.println("Multiplicacion: " + X*Y );
+        System.out.println("Division: " + X/Y );
+        System.out.println("Modulo: " + X%Y );  
+    }
+}
